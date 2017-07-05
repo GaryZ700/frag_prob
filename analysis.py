8d3da89e65860653995ec3b1c54a19f5fdb1d076
Gary <gzeri@uci.edu>
2017-07-05 12:21:59 -0700

    adds analysis.py

--- analysis.py
+++ analysis.py
@@ -1,31 +1,76 @@
-#script to run analysis on turbomole simulations
+#Script to perform in depth analysis on Turbomole Parser script data
 
-from tools.py import IO
+import os
 import argparse as ap
+from IO import IO
+from Molecule import Molecule
 
-#parse script arguemnts################################################################################################################################################################
-
-#-h: help, provide documetation on script
-
-#-ss: int, starting simulation, sumulation to start from, defualt = 0
-#-es: int, ending simulation, simulation to end from, defualt = max
-#-sm: int, starting md log, md log to start from, defualt = 0
-#-em: int, ending md log, md log to end form, defualt = max
-#-st: int, starting structure, structure to start from in md log, defualt - 0
-#-et: int, ending structure, structure to end on in md log, defualt = max
-
-#-ip: list of lists, individual parsing, list of type [[s,m,t],[s1,m1,t1],...[sn,mn,tn]], where s = simulation int, m = md log int, t = struct int
-#-ip: continued, s,m, and t can also be lists in themselves, ex. [ [ [1,2], [4,5,6], [1,2,3] ] ], would parse the first three data structures from the 4th, 5th, and 6th md logs of the first two simulations
-#-ip: continued, defualt = not used
-
-#-nf: t/f, new folder, save anlysis data to new folder, defualt = true
-#-o: string of anlysis folder, overwrite, save anlysis data to existing data file, defualt = false
-
-#-fn: string, folder name, name of anylysis folder, defualt = anlysisN, where n is the number of previous analyzations that have occured
+molecule = Molecule()
+io = IO()
 
 parser = ap.ArgumentParser()
 
+parser.add_argument('-f',metavar="folderHead",default="b4500m",type=str)
+
+args = parser.parse_args()
+
+####################################################################################
+
+def generateReport(folderHead):
+    #generate report on md simulations
+
+    #move into analysis folder
+    os.chdir("./analysis")
+
+    simulations = io.simulationRange(folderHead)
+
+    report = open("report",'w')
+
+    report.write("Turbomole Analysis Report")
+    
+    report.write("\n")
+
+    report.write("Simulation: " + folderHead + str(simulations[0]) + " to " + folderHead + str(simulations[1]))
+    
+    report.write(fragmentPathways(folderHead,simulations))
+
+    report.close()
 
 
-#initialize main IO for script
-io = IO()
+    #print report on screen
+    report = open("report",'r')
+    print(report.read())
+
+    #return to parent directory
+    os.chdir("..")
+
+
+###############################################################################
+def fragmentPathways(folderHead,simulations):
+    
+    fragments = []
+
+    for simulation in range(simulations[0],simulations[1] + 1):
+        
+        timesteps = molecule.load(folderHead + str(simulation))
+
+        for timestep in timesteps:
+            fragments += timestep["molecule"]["moleculeList"]
+    
+    return Counter(fragments)
+
+
+###############################################################################
+#main code goes here
+
+
+#check if analysis folder exists
+if(os.path.exists("./analysis")):
+    generateReport(args.f)
+else:
+    print("Can not perform analysis, analysis folder does not exist in current directory. Try to run mdparser.py before running analysis.py.")
+    
+    #data = molecule.load(str(folderHead + IO.simulationRange(folderHead)[1]))
+
+
+
